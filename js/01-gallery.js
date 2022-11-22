820bc4dd938f9fea0c35ac5f915860fbc83e59c5
epsilon11101 <amirandav1991@gmail.com>
2022-11-22 11:11:52 -0600

    event propagation

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const items = galleryItems
+  .map((item) => {
+    return `
+    <div class"gallery__item>
+        <a class"gallery__link" href="#">
+            <img class="gallery__image"
+            data-source="${item.original}"
+            src="${item.preview}"
+            alt="${item.description}"
+            />
+        </a>
+      </div>
+      `;
+  })
+  .join("");
+
+gallery.innerHTML = items;
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+});
